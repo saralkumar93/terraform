d17b96461dcd14e52c8bc7d019d16c8c2c5c545d
Saralk <saralkalwalwar@gmail.com>
2025-01-29 00:35:31 +0530

    update ec2 with key and sg

--- main.tf
+++ main.tf
@@ -1,9 +1,52 @@
 resource "aws_instance" "ec2" {
-    ami = "ami-00bb6a80f01f03502"
+   
+    ami = var.ec2-ubuntu-ami
     instance_type = "t2.micro"
     tags = {
       Name = "terraform-server"
+
     }
+    key_name = aws_key_pair.key.key_name
+
+    security_groups = [aws_security_group.allow_ssh.name]
+
+}
+
+resource "aws_key_pair" "key" {
+  key_name = "id_rsa"
+  public_key = file("~/ .ssh/id_rsa.pub")
+    
+}
+
+resource "aws_default_vpc" "default_vpc" {
+
+ }
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "allow ssh inbound traffic"
   
+
+  #using default vpc
+
+  vpc_id = aws_default_vpc.default_vpc.id
+  ingress {
+
+    description = "TLS from vpc"
+
+    #we should allow incoming and outgoing traffic
+
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+
+    #allow all traffic
+    cidr_blocks = ["0.0.0.0/0"]
+
+
+  }
+  tags = {
+    Name = "allow ssh"
+  }
 }
 
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ec2-ubuntu-ami" {
+    default = "ami-00bb6a80f01f03502"
+    description = "this is ubuntu ami id"
+  
+}
+variable "ec2-amazon-linux-ami" {
+    default = "ami-0b7207e48d1b6c06f"
+    description = "this is amazon-linux ami id"
+}
+
+variable "ec2-redhat-ami" {
+    default = "ami-02ddb77f8f93ca4ca"
+    description = "this is redhat ami id"
+}
